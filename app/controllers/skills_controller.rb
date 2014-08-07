2de50bc13b66fe896632bc3f4e89be0b08793ee9
ka.ryjov <ka.ryjov@ws1259.redoct.biz>
2014-08-07 16:50:37 +0400

    Skills page was edited and paginated

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,72 +1,15 @@
 class SkillsController < ApplicationController
-  before_action :set_skill, only: [:destroy]
-  before_action :set_parent, only: [:destroy, :create]
-
   # GET /skills
   # GET /skills.json
   def index
     if params[:query].nil?
-      @skills = Skill.all
+      @skills = Skill.
+                  #order(vacancies_count: :desc).
+                  paginate(:page => params[:page], :per_page => 5)
     else
       @skills = Skill.order(name: :asc).where(Skill.arel_table[:name].matches "%#{params[:query]}%").first(10)
-    end
-
-    @skill = Skill.new
-  end
-
-  # POST /skills
-  # POST /skills.json
-  def create
-    @skill = Skill.find_or_create_by(skill_params)
-
-    if @parent
-      @parent.skills << @skill unless @parent.skills.exists? @skill
-      respond_to do |format|
-        format.html { redirect_to [:edit, @parent] , notice: 'Умение добавлено.' }
-      end
-    else
-      respond_to do |format|
-        unless @skill.errors.any?
-          format.html { redirect_to skills_url, notice: "Умение \"#{@skill.name}\" создано." }
-          format.json { render :show, status: :created, location: @skill }
-        else
-          format.html { render :new }
-          format.json { render json: @skill.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  end
-
-  # DELETE /skills/1
-  # DELETE /skills/1.json
-  def destroy
-    if @parent
-      @parent.skills.delete(@skill)
-      respond_to do |format|
-        format.html { redirect_to [:edit, @parent], notice: 'Умение удалено.' }
-      end
-    else
-      @skill.destroy
-      respond_to do |format|
-        format.html { redirect_to skills_url, notice: 'Умение удалено.' }
-        format.json { head :no_content }
-      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_skill
-      @skill = Skill.find(params[:id])
-    end
-
-    def set_parent
-      @parent = Employee.find(params[:employee_id]) if params[:employee_id]
-      @parent = Vacancy.find(params[:vacancy_id]) if params[:vacancy_id]
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def skill_params
-      params.require(:skill).permit(:name)
-    end
 end
