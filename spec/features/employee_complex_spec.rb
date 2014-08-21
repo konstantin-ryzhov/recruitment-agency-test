976e29ad29ad03e3869f6d0d50e699a336f8f5b1
ka.ryjov <ka.ryjov@ws1259.redoct.biz>
2014-08-21 14:29:47 +0400

    feature specs were added

--- spec/features/employee_complex_spec.rb
+++ spec/features/employee_complex_spec.rb
@@ -1,37 +1,61 @@
 require 'rails_helper'
 
 feature 'Employee managment' do
-  scenario 'User creates a new employee' do
+
+  scenario 'Show employee' do
+    employee_skills = ['Ruby','Rails']
+    employee = create :employee, skills_array: employee_skills
+    visit employee_path(employee)
+
+    expect(page).to have_text(employee.name)
+    expect(page).to have_text(employee.contacts)
+    expect(page).to have_text(I18n.t employee.state)
+    expect(page).to have_text(employee.salary)
+
+    employee_skills.each do |s|
+      expect(page).to have_text(s)
+    end
+  end
+
+  scenario 'Remove employee' do
+    employee = create :employee
+    visit employee_path(employee)
+    click_on 'Удалить'
+    expect(page).to have_text('Работник удален')
+    expect(Employee.find_by_id employee.id).to be_nil
+  end
+
+  scenario 'Create a new employee' do
     visit '/employees'
-    save_and_open_page
 
     expect(page).to have_text('Работники')
-    find_link('Добавить').click
-    save_and_open_page
+    click_on 'Добавить'
    
     expect(page).to have_text('Новый работник')
     employee_name = 'Иванов Иван Иваныч'
     employee_contacts = 'test@test.ru'
     employee_state = 'Трудоустроен'
     employee_salary = '100000'
+    #умения для добавления
+    # employee_skills = ['Ruby','Rails']
 
     fill_in 'employee_name', :with => employee_name
     fill_in 'employee_contacts', :with => employee_contacts
     choose employee_state
     fill_in 'employee_salary', :with => employee_salary
-
     # добавить умения
+    # fill_in 'employee[skills_array]', :with => employee_skills
 
-    click_button 'Сохранить'
-    save_and_open_page
+    click_on 'Сохранить'
 
     expect(page).to have_text('Работник создан')
     expect(page).to have_text(employee_name)
     expect(page).to have_text(employee_contacts)
     expect(page).to have_text(employee_state)
     expect(page).to have_text(employee_salary)
-
     # проверить добавленные умения
-
+    # employee_skills.each do |s|
+    #   expect(page).to have_text(s)
+    # end
   end
 end
--- spec/features/vacancy_complex_spec.rb
+++ spec/features/vacancy_complex_spec.rb
@@ -1,37 +1,55 @@
 require 'rails_helper'
 
 feature 'Vacancy managment' do
-  scenario 'User creates a new vacancy' do
+  scenario 'Show vacancy' do
+    vacancy_skills = ['Ruby','Rails']
+    vacancy = create :vacancy, skills_array: vacancy_skills
+    visit vacancy_path(vacancy)
+
+    expect(page).to have_text(vacancy.name)
+    expect(page).to have_text(I18n.l vacancy.valid_until.to_date)
+    expect(page).to have_text(vacancy.salary)
+    expect(page).to have_text(vacancy.contacts)
+
+    vacancy_skills.each do |s|
+      expect(page).to have_text(s)
+    end
+  end
+
+  scenario 'Remove vacancy' do
+    vacancy = create :vacancy
+    visit vacancy_path(vacancy)
+    click_on 'Удалить'
+    expect(page).to have_text('Вакансия удалена')
+    expect(Vacancy.find_by_id vacancy.id).to be_nil
+  end
+
+  scenario 'Create a new vacancy' do
     visit '/vacancies'
-    save_and_open_page
 
     expect(page).to have_text('Вакансии')
-    find_link('Добавить').click
-    save_and_open_page
+    click_on 'Добавить'
    
     expect(page).to have_text('Новая вакансия')
     vacancy_name = 'test test test'
     valid_until = '01.11.2014'
     vacancy_salary = '100000'
     vacancy_contacts = 'test@test.ru'
+    #умения для добавления
 
     fill_in 'vacancy_name', :with => vacancy_name
     fill_in 'valid_until', :with => valid_until
     fill_in 'vacancy_salary', :with => vacancy_salary
     fill_in 'vacancy_contacts', :with => vacancy_contacts
-
     # добавить умения
 
-    click_button 'Сохранить'
-    save_and_open_page
+    click_on 'Сохранить'
 
     expect(page).to have_text('Вакансия создана')
     expect(page).to have_text(vacancy_name)
     expect(page).to have_text(valid_until)
     expect(page).to have_text(vacancy_salary)
     expect(page).to have_text(vacancy_contacts)
-
     # проверить добавленные умения
-
   end
 end
